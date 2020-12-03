ccffa44ebd3f97eed81b94e95cbc75b635050523
Diego Luiz <diego@magikal.com.br>
2020-12-03 11:35:34 -0300

    first commit

---
+++ src/boot/gmaps.js
@@ -0,0 +1,7 @@
+import { x5GMaps, gmaps } from 'x5-gmaps'
+
+// "async" is optional;
+// more info on params: https://quasar.dev/quasar-cli/boot-files
+export default async ({ app, router, Vue }) => {
+  // Vue.use(x5GMaps, { key: 'XXXXXX', libraries: ['places'] })
+}
---
+++ src/pages/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="flex flex-center">
+
+    <div style="width: 700px; height: 70vh">
+      <gmaps-map>
+        <gmaps-marker :position="position" />
+      </gmaps-map>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+
+import { x5GMaps, gmapsMarker, gmaps } from 'x5-gmaps'
+import Vue from 'vue'
+
+export default {
+  components: { 'gmaps-map': gmaps, gmapsMarker },
+  mounted () {
+    gmaps.then((response) => {
+      console.log('response', response)
+    })
+  },
+  data () {
+    return {
+      position: { lat: -27, lng: 153 }
+    }
+  }
+}
+</script>
